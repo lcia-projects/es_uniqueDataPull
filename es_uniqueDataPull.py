60d07e4598d578eda8ab87a2a61d5836b2700e32
darrellmiller <darrellrhodesmiller@gmail.com>
2021-04-05 22:17:40 -0500

    minor fixes with output folder

--- es_uniqueDataPull.py
+++ es_uniqueDataPull.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
       esQueryObj.saveUniques(OUTPUT_FOLDER, dataPull_Config['IGNORE_LIST'])
 
    #hour=00 #for testing
-   if hour==1:
+   if hour==4:
       for item in dataPull_Config['ES_FIELDLIST_DAILY']:
          print("Querying: ", item)
          esQueryObj.PullUniques(field_name=item)
